36598bc0593ece1d054e34ad5bda30cbfb95223a
vamiec <85052215+vamiec@users.noreply.github.com>
2021-11-29 08:35:20 +0530

    Update HelloWorld.java

--- src/net/codejava/HelloWorld.java
+++ src/net/codejava/HelloWorld.java
@@ -3,8 +3,9 @@ package net.codejava;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World 29!!!!");
+		System.out.println("Hello World Blue!!!!");
 
 	}
 
 }
+
